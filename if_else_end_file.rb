76f5ea389854dfa50c40f4a7b7cb6caaab6a3984
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-13 23:04:45 +0000

    Automatically backed up by Learn

--- if_else_end_file.rb
+++ if_else_end_file.rb
@@ -17,4 +17,29 @@ else
   puts "Enjoy the fine day"
 end
 puts "And always wear sunscreem"
-  
+
+
+#if,elsif, else, end
+
+chance_of_rain = 0.2
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter"
+  elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umberella"
+else
+  puts "Stay home and read Hegel."
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+

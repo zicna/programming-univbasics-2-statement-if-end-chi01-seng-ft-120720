b2858e1ddd76d83deb28cf571d1268462c6b4182
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-13 23:26:25 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -4,7 +4,7 @@ current_time = Time.now
 current_time = current_time.to_i
 puts current_time
 
-if (current_time.to_i % 2 == Integer)
+if (current_time.to_i % 2 == 0)
   puts "Even!"
 else 
   puts "Odd"

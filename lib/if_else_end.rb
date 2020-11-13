87f85e26313ae0cb40a7e3829707af78cceb56dd
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-13 23:18:58 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,3 @@
 # Write your solution here
 
-run_code_inside = true
-puts "Code before if .. end"
-if run_code_inside
-  puts "code inside"
-end 
-puts "Code afte if ... end"
+current_time = Time.now

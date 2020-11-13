c10863fa852f6a85735e62950c8b466d49d31210
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-13 22:59:30 +0000

    Automatically backed up by Learn

--- if_else_end_file.rb
+++ if_else_end_file.rb
@@ -1,6 +1,20 @@
+#if end statement
+
 run_code_inside = false
 puts "Code before if .. end"
 if run_code_inside
   puts "code inside"
 end 
 puts "Code after if ...end"
+
+#if, else, end
+
+chance_of_rain = 0.2
+puts "Let's go outside"
+if chance_of_rain > 0.5
+  puts "Pack an umberella!"
+else 
+  puts "Enjoy the fine day"
+end
+puts "And always wear sunscreem"
+  

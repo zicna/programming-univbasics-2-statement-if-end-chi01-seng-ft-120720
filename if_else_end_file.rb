6e4d22efa2dbaa10b22a2846c3a996d129f93275
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-13 23:06:21 +0000

    Automatically backed up by Learn

--- if_else_end_file.rb
+++ if_else_end_file.rb
@@ -2,7 +2,7 @@
 
 #if,elsif, else, end
 
-chance_of_rain = 0.2
+chance_of_rain = 0.00000001
 if chance_of_rain <= 0.25
   puts "Pack a sun shelter"
   elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
